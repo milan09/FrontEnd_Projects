0e06a82582c69e767f78f57e94ed6d495a0eda00
milan09 <milanshettigar@gmail.com>
2021-02-01 21:30:40 +0530

    add event listeners interaction

--- FourInARowGame/js/app.js
+++ FourInARowGame/js/app.js
@@ -3,4 +3,9 @@ const game = Game();
 /** 
  * Listens for click on `#begin-game` and calls startGame() on game object
  */
-document.getElementById('#begin-game')
+document.getElementById('#begin-game').addEventListener( 'click', function() {
+    game.startGame();
+
+    this.style.display = 'none';
+    document.getElementById('play-area').style.opacity = '1';
+});
